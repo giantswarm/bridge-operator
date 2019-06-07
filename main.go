816691d73958ee3ad8ccbf7d1241b0f5508f3496
Tim Schindler <tim@giantswarm.io>
2019-06-07 13:44:08 +0200

    add operator structure (#3)

--- main.go
+++ main.go
@@ -1,4 +1,118 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/microkit/command"
+	microserver "github.com/giantswarm/microkit/server"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/viper"
+
+	"github.com/giantswarm/bridge-operator/flag"
+	"github.com/giantswarm/bridge-operator/server"
+	"github.com/giantswarm/bridge-operator/service"
+)
+
+var (
+	description string     = "The bridge-operator handles network bridges in KVM hardware machines."
+	f           *flag.Flag = flag.New()
+	gitCommit   string     = "n/a"
+	name        string     = "bridge-operator"
+	source      string     = "https://github.com/giantswarm/bridge-operator"
+)
+
 func main() {
+	err := mainError(context.Background())
+	if err != nil {
+		panic(fmt.Sprintf("%#v", err))
+	}
+}
+
+func mainError(ctx context.Context) error {
+	var err error
+
+	var newLogger micrologger.Logger
+	{
+		c := micrologger.Config{}
+
+		newLogger, err = micrologger.New(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	serverFactory := func(v *viper.Viper) microserver.Server {
+		var newService *service.Service
+		{
+			c := service.Config{
+				Flag:   f,
+				Logger: newLogger,
+				Viper:  v,
+
+				Description: description,
+				GitCommit:   gitCommit,
+				ProjectName: name,
+				Source:      source,
+			}
+
+			newService, err = service.New(c)
+			if err != nil {
+				panic(fmt.Sprintf("%#v", microerror.Mask(err)))
+			}
+
+			go newService.Boot(ctx)
+		}
+
+		var newServer microserver.Server
+		{
+			c := server.Config{
+				Logger:  newLogger,
+				Service: newService,
+				Viper:   v,
+
+				ProjectName: name,
+			}
+
+			newServer, err = server.New(c)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		return newServer
+	}
+
+	var newCommand command.Command
+	{
+		c := command.Config{
+			Logger:        newLogger,
+			ServerFactory: serverFactory,
+
+			Description:    description,
+			GitCommit:      gitCommit,
+			Name:           name,
+			Source:         source,
+			VersionBundles: service.NewVersionBundles(),
+		}
+
+		newCommand, err = command.New(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	daemonCommand := newCommand.DaemonCommand().CobraCommand()
+
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.Address, "http://127.0.0.1:6443", "Address used to connect to Kubernetes. When empty in-cluster config is created.")
+	daemonCommand.PersistentFlags().Bool(f.Service.Kubernetes.InCluster, false, "Whether to use the in-cluster config to authenticate with Kubernetes.")
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.KubeConfig, "", "KubeConfig used to connect to Kubernetes. When empty other settings are used.")
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.CAFile, "", "Certificate authority file path to use to authenticate with Kubernetes.")
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.CrtFile, "", "Certificate file path to use to authenticate with Kubernetes.")
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.KeyFile, "", "Key file path to use to authenticate with Kubernetes.")
+
+	newCommand.CobraCommand().Execute()
+
+	return nil
 }
---
+++ service/service.go
@@ -0,0 +1,117 @@
+package service
+
+import (
+	"context"
+	"sync"
+
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/microendpoint/service/version"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/client/k8srestconfig"
+	"github.com/spf13/viper"
+	"k8s.io/client-go/rest"
+
+	"github.com/giantswarm/bridge-operator/flag"
+	"github.com/giantswarm/bridge-operator/service/collector"
+)
+
+type Config struct {
+	Logger micrologger.Logger
+
+	Flag  *flag.Flag
+	Viper *viper.Viper
+
+	Description string
+	GitCommit   string
+	ProjectName string
+	Source      string
+}
+
+type Service struct {
+	Version *version.Service
+
+	bootOnce          sync.Once
+	operatorCollector *collector.Set
+}
+
+func New(config Config) (*Service, error) {
+	if config.Flag == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Flag must not be empty", config)
+	}
+	if config.Viper == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Viper must not be empty", config)
+	}
+
+	var err error
+
+	var restConfig *rest.Config
+	{
+		c := k8srestconfig.Config{
+			Logger: config.Logger,
+
+			Address:    config.Viper.GetString(config.Flag.Service.Kubernetes.Address),
+			InCluster:  config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster),
+			KubeConfig: config.Viper.GetString(config.Flag.Service.Kubernetes.KubeConfig),
+			TLS: k8srestconfig.ConfigTLS{
+				CAFile:  config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile),
+				CrtFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile),
+				KeyFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile),
+			},
+		}
+
+		restConfig, err = k8srestconfig.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	g8sClient, err := versioned.NewForConfig(restConfig)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var operatorCollector *collector.Set
+	{
+		c := collector.SetConfig{
+			G8sClient: g8sClient,
+			Logger:    config.Logger,
+		}
+
+		operatorCollector, err = collector.NewSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var versionService *version.Service
+	{
+		c := version.Config{
+			Description:    config.Description,
+			GitCommit:      config.GitCommit,
+			Name:           config.ProjectName,
+			Source:         config.Source,
+			VersionBundles: NewVersionBundles(),
+		}
+
+		versionService, err = version.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	s := &Service{
+		Version: versionService,
+
+		bootOnce:          sync.Once{},
+		operatorCollector: operatorCollector,
+	}
+
+	return s, nil
+}
+
+func (s *Service) Boot(ctx context.Context) {
+	s.bootOnce.Do(func() {
+		go s.operatorCollector.Boot(ctx)
+	})
+}
